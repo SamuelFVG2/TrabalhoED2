5be6bea3a216ce18300a7561a271bed7d5188357
SamuelFVG2 <felipegoncalvesbr2@gmail.com>
2026-06-23 22:39:33 -0300

    feat:add basic line reading functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
+#include <sstream>
 
 #include "./classes/department/department.h"
 
+Department getDepartmentFromLine(std::string line) {
+  std::stringstream ss(line);
+  std::string code;
+  std::string name;
+  std::string keyword;
+
+  ss >> code;
+  ss >> name;
+  Department department(code, name);
+  while (ss >> keyword) {
+    department.addWord(keyword);
+  }
+
+  return department;
+}
+
 int main() {
+  std::string line = "001 JURÍDICO INVESTIMENTO RENDA GASTOS DESPESAS LUCRO";
 
-    Department department("001", "FINANCEIRO");
+  Department department = getDepartmentFromLine(line);
 
-    department.addWord("INVESTIMENTO");
-    department.addWord("RENDA");
-    department.addWord("GASTOS");
-    department.addWord("DESPESAS");
-    department.addWord("LUCRO");
-
-    std::cout << department.getDepartment() << std::endl;
-
-    return 0;
+  std::cout << department.getDepartment() << std::endl;
+  return 0;
 }
